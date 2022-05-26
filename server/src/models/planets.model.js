1af3e29901471cdad7f69200a76de824cd7f377a
Stefano Proietti <stef.pro@hotmail.com>
2022-05-26 11:58:47 -0400

    added logic for parsing planets data, added scripts

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,38 @@
-const planets = [];
+const path = require('path');
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function isHabitable(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
+}
+
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, "..", "..", "data", 'kepler_data.csv'))
+        .pipe(parse({
+            comment: '#',
+            columns: true
+        }))
+        .on('data', (data) => {
+            if(isHabitable(data)) {
+                habitablePlanets.push(data);
+            }
+        })
+        .on('error', (err) => {
+            console.log(err);
+            reject(err);
+        })
+        .on('end', () => {
+            console.log(`There are ${habitablePlanets.length} habitable planets that have been found!`);
+            resolve();
+        });
+    })    
+}
+
+
+module.exports = {
+    loadPlanetsData,
+    planets: habitablePlanets,
+} 
