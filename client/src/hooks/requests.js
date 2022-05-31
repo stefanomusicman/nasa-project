ae7f3ca4d81b9d542843ecebf814064d06940b5c
Stefano Proietti <stef.pro@hotmail.com>
2022-05-31 11:46:16 -0400

    connected front and back for adding launches

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,6 +1,5 @@
 const API_URL = 'http://localhost:8000';
 
-// TODO: Once API is ready.
 // Load planets and return as JSON.
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
@@ -8,7 +7,6 @@ async function httpGetPlanets() {
   return data
 }
 
-// TODO: Once API is ready.
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`);
@@ -16,10 +14,21 @@ async function httpGetLaunches() {
   return data.sort((a, b) => a.flightNumber - b.flightNumber);
 }
 
-// TODO: Once API is ready.
 // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(launch),
+    })
+  } catch(err) {
+    return {
+      ok: false
+    }
+  }
 }
 
 // TODO: Once API is ready.
